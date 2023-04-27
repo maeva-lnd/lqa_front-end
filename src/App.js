6196a81225e395518295d7e804a8d0f4bdc8f533
Maëva Landreau <mavalandreau@gmail.com>
2023-04-27 19:10:55 +0200

    Add authentification pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from "./Components/Footer/Footer";
 import {Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import RestaurantMenu from "./Pages/RestaurantMenu/RestaurantMenu";
+import Login from "./Pages/Authentification/Login/Login";
+import ForgotPassword from "./Pages/Authentification/ForgotPassword/ForgotPassword";
+import CreateAccount from "./Pages/Authentification/CreateAccount/CreateAccount";
 
 function App() {
 
@@ -13,6 +16,9 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/lacarte" element={<RestaurantMenu />}/>
+            <Route path="/connexion" element={<Login />}/>
+            <Route path="/motdepasseperdu" element={<ForgotPassword />}/>
+            <Route path="/inscription" element={<CreateAccount />}/>
         </Routes>
         <Footer />
     </div>
