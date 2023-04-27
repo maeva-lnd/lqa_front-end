1f55417cbe49a62f0c212a3585a343969a05ba6c
Maëva Landreau <mavalandreau@gmail.com>
2023-04-27 18:05:52 +0200

    Add restaurant card page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import {Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import RestaurantMenu from "./Pages/RestaurantMenu/RestaurantMenu";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/lacarte" element={<RestaurantMenu />}/>
         </Routes>
         <Footer />
     </div>
